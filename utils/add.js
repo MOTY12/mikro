50e5fceeb4f7685efef662e0bd10c7691677372a
MOTY <50269513+MOTY12@users.noreply.github.com>
2022-01-05 02:04:03 +0100

    Update add.js
    Update mikro

--- utils/add.js
+++ utils/add.js
@@ -3,13 +3,44 @@ class addPhonebook {
         this.ContactList = []
     }
 
-
+    
     storeContact(addcontact) {
         this.addcontact = addcontact;
-        const moty = this.ContactList.push(addcontact);
-        console.log(moty)
+        let findPhonenumber = addcontact.phoneNumber
+        const response = this.ContactList.find(({phonenumber}) => phonenumber === findPhonenumber);
+        if (response) {
+            // remove contact
+            let index = this.ContactList.indexOf(response);
+            this.ContactList.splice(index, 1);
+        }
+         const moty = this.ContactList.push(addcontact);
+         console.log(moty)
+ 
+class addPhonebook {
+ 
+         if (phoneNumber) {
+             let findPhonenumber = phoneNumber
+           response = this.ContactList.find((Contact) => Contact.phonenumber === findPhonenumber);
+            response = this.ContactList.find(({phonenumber}) => phonenumber === findPhonenumber);
+             // console.log(response);
+         }
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    }
+
+
 
     fetchContact() {
         return this.ContactList
@@ -22,7 +53,7 @@ class addPhonebook {
             let findPhonenumber = phoneNumber
             response = this.ContactList.find((Contact) => Contact.phonenumber === findPhonenumber);
             // console.log(response);
-        }
+
     }
 
 }
